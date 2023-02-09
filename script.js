bb0c9904d7f368ddbc1677cc9166269b731b2ad0
Tudy1610 <47245909+Tudy1610@users.noreply.github.com>
2023-02-09 16:19:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    window.location = "explore.html";
+  }
+function recallFunction()
+  {
+    window.location = "antreprenoriala.html"
+  }
+function specSaltea()
+{
+  window.location = "specificatii.html"
+}
